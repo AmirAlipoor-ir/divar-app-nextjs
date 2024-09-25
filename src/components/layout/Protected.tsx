72190fb0cc114463a182bf7dbe3c04163e53cf4d
Amir <amir2005alipoor@gmail.com>
2024-09-25 10:48:37 +0330

    feat: add getExpireAccesData function

--- src/components/layout/Protected.tsx
+++ src/components/layout/Protected.tsx
@@ -4,10 +4,9 @@ import { ReactNode, useEffect } from "react";
 
 import Cookies from "js-cookie";
 
-import { jwtDecode } from "jwt-decode";
-
 import { useChekRefreshTokenMutation } from "@/services/login";
-import { CheckOtpRes } from "../CheckOtp/types";
+
+import { getExpireAccessDate } from "@/utils/cookies";
 
 export const Protected = ({ children }: { children: ReactNode }) => {
   const refreshTokenCookie = Cookies.get("refreshTokenCookie");
@@ -20,29 +19,26 @@ export const Protected = ({ children }: { children: ReactNode }) => {
     const fetchData = async () => {
       const now = Math.floor(Date.now() / 1000);
 
-      const getExpireDate = (token: string) => {
-        const { exp } = jwtDecode(token);
-
-        return exp;
-      };
-
       if (!accessTokenCookie && refreshTokenCookie) {
         const data = await getNewToken(refreshTokenCookie).unwrap();
 
-        const accessTokenExpireDate: any = getExpireDate(data.accessToken);
+        const accessTokenExpireDate: any = getExpireAccessDate(
+          data.accessToken
+        );
 
         Cookies.set("accessTokenCookie", data.accessToken, {
           expires: new Date(accessTokenExpireDate * 1000),
         });
       } else if (accessTokenCookie) {
-        const accessTokenExpireDate: any = getExpireDate(accessTokenCookie);
+        const accessTokenExpireDate: any =
+          getExpireAccessDate(accessTokenCookie);
 
         if (accessTokenExpireDate - now <= 5) {
           if (refreshTokenCookie) {
             const data = await getNewToken(refreshTokenCookie);
 
             if (data.data?.accessToken) {
-              const newAccessTokenExpireDate: any = getExpireDate(
+              const newAccessTokenExpireDate: any = getExpireAccessDate(
                 data.data.accessToken
               );
 
--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -4,6 +4,12 @@ import { jwtDecode } from "jwt-decode";
 
 import { CheckOtpRes, DecodedToken } from "./type";
 
+export const getExpireAccessDate = (token: string) => {
+  const { exp } = jwtDecode(token);
+
+  return exp;
+};
+
 export const setCookies = ({ data }: { data: CheckOtpRes }) => {
   const getExpireDate = (token: string) => {
     const expInSeconds = jwtDecode<DecodedToken>(token).exp;
