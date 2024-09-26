691c1d6b122dea0bebb6687e0a66c740b5280483
Amir <amir2005alipoor@gmail.com>
2024-09-26 08:49:00 +0330

    refactor: add toster from navbar to provider

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -4,7 +4,13 @@ import { ReactNode } from "react";
 
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "@/redux-toolkit/store";
+import { Toaster } from "react-hot-toast";
 
 export const Provider = ({ children }: { children: ReactNode }) => {
-  return <ReduxProvider store={store}>{children}</ReduxProvider>;
+  return (
+    <ReduxProvider store={store}>
+      <Toaster />
+      {children}
+    </ReduxProvider>
+  );
 };
