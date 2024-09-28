ff7521860e1432347bcf4afc9a1a0947a8a565cd
Amir <amir2005alipoor@gmail.com>
2024-09-28 08:40:17 +0330

    feat: add style and add the category list in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useGetCategoryQuery } from "@/services/category";
+
 export default function Home() {
   const posters = [
     {
@@ -8,12 +11,17 @@ export default function Home() {
     {
       title: "car",
       price: "56",
-      city: "qpm",
+      city: "qazvin",
     },
     {
       title: "ben",
       price: "12000",
       city: "gilan",
+    },
+    {
+      title: "car",
+      price: "56",
+      city: "qazvin",
     },
     {
       title: "car",
@@ -30,15 +38,39 @@ export default function Home() {
       price: "1235",
       city: "qazvin",
     },
+    {
+      title: "car",
+      price: "56",
+      city: "qazvin",
+    }, {
+      title: "car",
+      price: "56",
+      city: "qazvin",
+    },
   ];
 
+  const data: any = useGetCategoryQuery();
+
   return (
-    <div>
-      <div className="flex flex-wrap gap-9 justify-center ">
-        {posters.map((item) => (
+    <div className="mt-10 flex justify-between gap-x-3">
+      <section className="w-1/5">
+        <h1 className="font-black text-2xl mb-5">Categories List</h1>
+        <div>
+          {data.data?.map(
+            ({ name, icon }: { name: string; icon: string }, index: number) => (
+              <div className="flex mb-3 gap-x-2 border-2 rounded-lg p-1" key={index}>
+                <img src={`/${icon}.svg`} alt="icon" />
+                <span>{name}</span>
+              </div>
+            )
+          )}
+        </div>
+      </section>
+      <div className="flex flex-wrap gap-9 justify-center w-4/5 ">
+        {posters.map((item,index) => (
           <div
             className="border-2 rounded-md flex p-3 max-w-72"
-            key={item.title}
+            key={index}
           >
             {/* <Image
               className="rounded-md"
