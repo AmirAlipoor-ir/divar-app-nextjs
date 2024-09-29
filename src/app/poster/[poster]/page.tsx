7dcd5055d8f5df75f99889117dea33a87da39371
Amir <amir2005alipoor@gmail.com>
2024-09-29 13:26:48 +0330

    feat: add handleGoBack function

--- src/app/poster/[poster]/page.tsx
+++ src/app/poster/[poster]/page.tsx
@@ -1,26 +1,36 @@
 "use client";
 
 import { useGetPosterQuery } from "@/services/poster";
+import { useRouter } from "next/navigation";
 
 const ReviewDetailPage = ({ params }: any) => {
+  const router = useRouter();
+
   const { data } = useGetPosterQuery();
+
   const posterId = params.poster;
-  // console.log(posterId);
-  // console.log(data?.posts.);
 
   const posterDetail = data?.posts.find(
     ({ _id }: { _id: string }) => _id === posterId
   );
-  console.log(posterDetail);
+  const handleGoBack = () => {
+    router.push("/");
+  };
   return (
-    <>
+    <div>
       <div className="border-2 mt-4 rounded-lg text-3xl flex justify-between p-4">
-        <span>title: {posterDetail.options.title}</span>
-        <span>price: {posterDetail.amount}</span>
+        <span>title: {posterDetail?.options.title}</span>
+        <span>price: {posterDetail?.amount}</span>
         {/* <span>price: {posterDetail.options.title}</span> */}
-        <span>city: {posterDetail.options.city}</span>
+        <span>city: {posterDetail?.options.city}</span>
       </div>
-    </>
+      <div
+        onClick={handleGoBack}
+        className="text-xl mt-10 p-4 w-52 rounded-xl bg-red-50 border-2 cursor-pointer"
+      >
+        back to the posters
+      </div>
+    </div>
   );
 };
 
