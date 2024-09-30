a1eac5a58d8d43d851b7a0a38f03719c3172cb99
Amir <amir2005alipoor@gmail.com>
2024-09-30 11:12:41 +0330

    refactor: add react hook form in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { useRouter } from "next/navigation";
 
 import Cookies from "js-cookie";
+
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import toast from "react-hot-toast";
 
 import { useGetCategoryQuery } from "@/services/category";
 
@@ -12,46 +16,36 @@ import { useAddPosterMutation } from "@/services/poster";
 
 import { MyPost } from "@/components/MyPost";
 
+import { PosterRes } from "../type";
+
 export default function DashboardPage() {
+  const { register, handleSubmit, reset } = useForm<PosterRes>();
+
   const router = useRouter();
-
-  const [title, setTitle] = useState("");
-
-  const [content, setContent] = useState("");
-
-  const [amount, setAmount] = useState("");
-
-  const [city, setCity] = useState("");
-
-  const [category, setCategory] = useState("");
 
   const [createPoster] = useAddPosterMutation();
 
   const { data } = useGetCategoryQuery();
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    createPoster({ title, content, amount, city, category });
+  const onSubmit: SubmitHandler<PosterRes> = async (data) => {
+    try {
+      await createPoster({
+        title: data.title,
+        content: data.content,
+        amount: data.amount,
+        city: data.city,
+        category: data.category,
+      });
+      reset();
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("An unexpected error occurred.");
+      }
+    }
   };
 
-  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
-  };
-
-  const handleChangeContent = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setContent(e.target.value);
-  };
-
-  const handleChangeAmount = (e: ChangeEvent<HTMLInputElement>) => {
-    setAmount(e.target.value);
-  };
-
-  const handleChangeCity = (e: ChangeEvent<HTMLInputElement>) => {
-    setCity(e.target.value);
-  };
-  const handleChangeCategory = (e: ChangeEvent<HTMLSelectElement>) => {
-    setCategory(e.target.value);
-  };
   const token = Cookies.get("accessTokenCookie");
   useEffect(() => {
     if (!token) router.push("/login");
@@ -60,47 +54,39 @@ export default function DashboardPage() {
   return (
     <div>
       <div className="flex justify-between">
-        <form onSubmit={handleSubmit} className="flex flex-col w-1/2">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-1/2">
           <h1 className="text-3xl mt-3 font-bold">Add Poster</h1>
           <hr className="w-44 border-red-500 border-2 mb-5" />
 
           <label className="posterLabel">title</label>
           <input
-            onChange={handleChangeTitle}
-            value={title}
+            {...register("title", { required: true })}
             type="text"
             className="posterInput"
           />
 
           <label className="posterLabel">information</label>
           <textarea
-            onChange={handleChangeContent}
-            value={content}
+            {...register("content", { required: true })}
             className="posterInput"
           />
 
           <label className="posterLabel">price</label>
           <input
-            onChange={handleChangeAmount}
-            value={amount}
+            {...register("amount", { required: true })}
             type="number"
             className="posterInput"
           />
 
           <label className="posterLabel">city</label>
           <input
-            onChange={handleChangeCity}
-            value={city}
+            {...register("city", { required: true })}
             type="text"
             className="posterInput"
           />
 
           <label className="posterLabel">Chategory:</label>
-          <select
-            value={category}
-            className="posterSelect"
-            onChange={handleChangeCategory}
-          >
+          <select className="posterSelect" {...register("category")}>
             {data?.map(({ name }: { name: string }, index: number) => (
               <option key={index} value={name}>
                 {name}
@@ -108,7 +94,7 @@ export default function DashboardPage() {
             ))}
           </select>
           <button
-            disabled={!title || !amount}
+            // disabled={!title || !amount}
             type="submit"
             className="border-2 w-full mt-10 rounded-lg py-3 text-white bg-red-500 text-2xl disabled:bg-slate-500"
           >
--- src/app/type.d.ts
+++ src/app/type.d.ts
@@ -2,3 +2,11 @@ export type FormData = {
   name: string;
   icon: string;
 };
+
+export type PosterRes = {
+  title: string;
+  content: string;
+  city: string;
+  category: string;
+  amount: string;
+};
